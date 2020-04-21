cb4e218dfc562fd5425f166bb1ebe79075a91cf4
Gabriel Araujo <gabrielbradoki@gmail.com>
2020-04-21 14:49:51 -0300

    Gerador de senhas em Swift

--- radomApp/ViewController.swift
+++ radomApp/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        let numero = arc4random_uniform(100000000)
+        
+        legendaResultado.text = String(numero)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
